3f871e61ecd18b6b3b165feb0a53eeff60fab2b3
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2026-03-01 12:39:53 -0300

    cleanup unecessary files, rename doc file to .md

--- src/async-trace.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test, describe } from "vitest";
-import { BehaviourTree } from "./tree";
-import { AsyncAction } from "./base/async-action";
-import { ref } from "./base/ref";
-import { NodeResult } from "./base/types";
-
