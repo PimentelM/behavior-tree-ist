bdf7d2ba31d7eac4979d94adb792b9dc199c6cea
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2026-03-03 16:37:06 +0000

    add required deps and change vitest config

--- packages/studio-server/vitest.config.ts
+++ packages/studio-server/vitest.config.ts
@@ -4,5 +4,13 @@ export default defineConfig({
   test: {
     include: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
     passWithNoTests: true,
+    testTimeout: 15000,
+    pool: 'forks',
+    fileParallelism: false,
+    server: {
+      deps: {
+        inline: ['knex'],
+      },
+    },
   },
 })
