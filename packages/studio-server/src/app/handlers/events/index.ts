af005e680e3a66ea3bd3302cae2ba61f45277039
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2026-03-04 03:54:06 -0300

    broadcast ui events to connected ui clients

--- packages/studio-server/src/app/handlers/events/index.ts
+++ packages/studio-server/src/app/handlers/events/index.ts
@@ -1,5 +1,37 @@
-import { AppDependencies } from "../../../types";
+import { UiMessageType } from '@behavior-tree-ist/studio-common';
+import type { AppDependencies } from '../../../types';
 
-export function registerLocalDomainEventHandlers(_appDependencies: AppDependencies) {
+export function registerLocalDomainEventHandlers({
+    uiWsServer,
+    eventDispatcher,
+}: Pick<AppDependencies, 'uiWsServer' | 'eventDispatcher'>): void {
+    eventDispatcher.on('Agent', 'AgentConnected', async ({ event }) => {
+        const { clientId, sessionId } = event.body;
 
+        uiWsServer.broadcast({
+            t: UiMessageType.AgentOnline,
+            clientId,
+            sessionId,
+        });
+    });
+
+    eventDispatcher.on('Agent', 'AgentDisconnected', async ({ event }) => {
+        const { clientId, sessionId } = event.body;
+
+        uiWsServer.broadcast({
+            t: UiMessageType.AgentOffline,
+            clientId,
+            sessionId,
+        });
+    });
+
+    eventDispatcher.on('Agent', 'CatalogChanged', async ({ event }) => {
+        const { clientId, sessionId } = event.body;
+
+        uiWsServer.broadcast({
+            t: UiMessageType.CatalogChanged,
+            clientId,
+            sessionId,
+        });
+    });
 }
